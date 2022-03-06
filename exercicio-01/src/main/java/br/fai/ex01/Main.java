bc71504f1507167a9eb5371e73b0d65a99f1254b
Rodolfo Mamud <rodolfo@pdsolucoes.com.br>
2022-03-06 20:17:36 -0300

    feat: adicionado estrutura do projeto

--- exercicio-01/src/main/java/br/fai/ex01/Main.java
+++ exercicio-01/src/main/java/br/fai/ex01/Main.java
@@ -1,5 +1,10 @@
 package br.fai.ex01;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
 		Main app = new Main();
@@ -7,6 +12,15 @@ public class Main {
 	}
 	
 	private void start() {
+		
+		List<Integer> votos = new ArrayList<Integer>();
+		Scanner scanner = new Scanner(System.in);
+		
+		while (true) {
+			int voto;
+			
+		}
+		
 		
 	}
 }
